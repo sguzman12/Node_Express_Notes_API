47a099344e1d3a6a3ee5289a60190dac025c0cb0
Sigfredo Guzman <sigfredo.guzman@leidos.com>
2022-04-14 13:27:15 -0400

    Created POST to Txt and Json files. Still need to add Status codes.

--- notes-app/util/fileWrite.js
+++ notes-app/util/fileWrite.js
@@ -1,12 +1,45 @@
 import fs from "fs";
 import readline from "readline";
+import { readJsonFile } from "./fileRead.js";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+export const insertNoteIntoTxtFile = (item) => {
+  fs.appendFileSync("./repo/notes.txt", `${item}\n`);
+};
 
-rl.question(`Please enter a note: `, (item) => {
-  fs.appendFileSync("../repo/notes.txt", `${item}\n`);
-  rl.close();
-});
+export const consoleInsertTxtFile = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question(`Please enter a note: `, (item) => {
+    fs.appendFileSync("../repo/notes.txt", `${item}\n`);
+    rl.close();
+  });
+};
+
+export const insertNoteIntoJsonFile = (item) => {
+  if (item != "") {
+    const jsonPromise = readJsonFile();
+
+    jsonPromise.then((data) => {
+      const parsedData = JSON.parse(data);
+
+      parsedData?.data.push({ message: item });
+
+      fs.writeFile(
+        "./repo/notes.json",
+        JSON.stringify(parsedData, null, 2),
+        "utf-8",
+        (err) => {
+          if (err) {
+            console.log(err);
+            // return 409;
+          } else {
+            // return 201;
+          }
+        }
+      );
+    });
+  }
+};
